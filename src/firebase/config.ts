25dc5c6a2955a01e0f222032d808d5f257615361
220v Soluções elétricas LTDA <eletrica220v.caruaru@gmail.com>
2025-11-11 19:01:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2525054452-63803",
+  "appId": "1:298685040405:web:6aa40cdf3b78848e3acb1c",
+  "apiKey": "AIzaSyCw0KqTfFagLA4hAe289hUpGGVMer8aXDs",
+  "authDomain": "studio-2525054452-63803.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "298685040405"
+};
