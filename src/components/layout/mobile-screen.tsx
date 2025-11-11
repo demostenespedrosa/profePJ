da00595693d0a29b3faae3e5bddd7e66a4d1f2e5
220v Soluções elétricas LTDA <eletrica220v.caruaru@gmail.com>
2025-11-11 14:00:08 +0000

    em modo desktop. o sistema fica com mobile. vamos fazer com que seja res

--- src/components/layout/mobile-screen.tsx
+++ src/components/layout/mobile-screen.tsx
@@ -8,9 +8,9 @@ type MobileScreenProps = {
 
 export default function MobileScreen({ children, className }: MobileScreenProps) {
   return (
-    <div className="h-dvh w-full flex items-center justify-center">
+    <div className="flex justify-center w-full min-h-dvh">
       <div className={cn(
-        "relative flex flex-col h-full w-full max-w-sm bg-background shadow-2xl md:h-[95vh] md:max-h-[800px] md:rounded-[3rem] md:border-8 md:border-gray-800 dark:md:border-gray-600",
+        "relative flex flex-col w-full bg-background",
         className
       )}>
         {children}
